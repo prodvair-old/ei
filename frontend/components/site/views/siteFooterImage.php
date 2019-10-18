e6aa0caf2a91fdbca38601a131af46bae4fb63ca
prodvair <far.almazi@gmail.com>
2019-10-18 06:18:05 +0300

    add contact and service

---
+++ frontend/components/site/views/siteFooterImage.php
@@ -0,0 +1,21 @@
+<section class="border-top pv-30">
+			
+    <div class="row gap-20 gap-lg-40 align-items-center justify-content-center line-1">
+        <div class="col-auto">
+            <div class="image"><img src="img/image-logo/logo-sm-bl-03.png" alt="image" /></div>
+        </div>
+        <div class="col-auto">
+            <div class="image"><img src="img/image-logo/logo-sm-bl-04.png" alt="image" /></div>
+        </div>
+        <div class="col-auto">
+            <div class="image"><img src="img/image-logo/logo-sm-bl-05.png" alt="image" /></div>
+        </div>
+        <div class="col-auto">
+            <div class="image"><img src="img/image-logo/logo-sm-bl-07.png" alt="image" /></div>
+        </div>
+        <div class="col-auto">
+            <div class="image"><img src="img/image-logo/logo-sm-bl-08.png" alt="image" /></div>
+        </div>
+    </div>
+
+</section>
