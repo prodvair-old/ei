51cd6ceb97a28e8d6da84e08cf9b2fbeaf51f3a2
prodvair <33940999+prodvair@users.noreply.github.com>
2019-11-15 17:29:39 +0400

    make a 404 page

--- frontend/views/site/error.php
+++ frontend/views/site/error.php
@@ -9,19 +9,24 @@ use yii\helpers\Html;
 
 $this->title = $name;
 ?>
+<section class="error">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-12 text-center">
+                <h1 class="error__header">Ошибка 404</h1>
+                <p class="error__text">страница не найдена</p>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-4 offset-lg-4 text-center">
+                <a href="/" class="error__btn">Вернуться на главную</a>
+            </div>
+        </div>
+    </div>
+</section>
 <div class="site-error">
 
-    <h1><?= Html::encode($this->title) ?></h1>
 
-    <div class="alert alert-danger">
-        <?= nl2br(Html::encode($message)) ?>
-    </div>
-
-    <p>
-        The above error occurred while the Web server was processing your request.
-    </p>
-    <p>
-        Please contact us if you think this is a server error. Thank you.
-    </p>
+    
 
 </div>
