974c2615e7299ad0295670a2641be0e2f75304e1
Lenar <lenar21.97@gmail.com>
2020-02-06 16:03:23 +0300

    pull cha

--- gulpfile.js
+++ gulpfile.js
@@ -37,7 +37,7 @@ gulp.task("browser-sync-server", function() {
       port: 3002
     },
     baseDir: "frontend/web/",
-    proxy: "http://dev-ei.test",
+    proxy: "http://ei/",
     port: 3005,
     ghostMode: {
       codeSync: false,
