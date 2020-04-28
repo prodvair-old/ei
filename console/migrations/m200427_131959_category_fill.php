4cc0b2b57861ef5dacfac25515aea0955938c189
Sergey Morozov <sergmoro1@ya.ru>
2020-04-28 10:13:36 +0300

    Replace duplicate Category ID's.

--- console/migrations/m200427_131959_category_fill.php
+++ console/migrations/m200427_131959_category_fill.php
@@ -6,22 +6,52 @@ use common\models\db\Category;
 
 /**
  * Class m200427_131959_category_fill
+ * Заполнение нового справаочника категорий.
  */
 class m200427_131959_category_fill extends Migration
 {
     const TABLE = '{{%category}}';
 
+    private $properties = [10000 => 'bankrupt', 20000 => 'arrest', 30000 => 'zalog'];
+
     public function safeUp()
     {
+        // создание корневой категории
+        $category = new Category(['id' => Category::ROOT, 'name' => 'Все категории', 'slug' => 'lot-list']);
+        $category->makeRoot();
+
+        // получение категорий из существующего справочника
         $db = \Yii::$app->db;
         $select = $db->createCommand(
             'SELECT * FROM site."lotsCategory" WHERE id > ' . Category::ROOT
         );
-        $category = new Category(['id' => Category::ROOT, 'name' => 'Все категории', 'slug' => 'lot-list']);
-        $category->makeRoot(); 
-        foreach($select->queryAll() as $row) {
+        $rows = $select->queryAll();
+        
+        // сохранение ID всех категорий первого уровня, чтобы не пытаться добавить категорию с таким же ID
+        $all_ids = [];
+        foreach($rows as $row) {
+            $all_ids[] = $row['id'];
+        }
+
+        // добавление категорий в новый справочник
+        foreach($rows as $row) {
             $model = new Category(['id' => $row['id'], 'name' => $row['name'], 'slug' => $row['translit_name']]);
             $model->appendTo($category);
+            foreach($this->properties as $step => $property) {
+                if ($row[$property . '_categorys']) {
+                    $objs = json_decode($row[$property . '_categorys']);
+                    foreach($objs as $id => $obj) {
+                        $id = (int) $id;
+                        // увеличиваем ID категорий, которые уже существуют
+                        if (in_array($id, $all_ids)) {
+                            $id += $step;
+                        }
+                        $m = new Category(['id' => $id, 'name' => $obj->name, 'slug' => $obj->translit]);
+                        $m->appendTo($model);
+                        $all_ids[] = $id;
+                    }
+                }
+            }
         }
     }
 
