758e26892ed620ecb21a46a1608da34c45d4a329
prodvair <far.almazi@gmail.com>
2020-05-07 09:44:55 +0300

    added etp

--- console/migrations/m200507_061808_etp.php
+++ console/migrations/m200507_061808_etp.php
@@ -18,24 +18,17 @@ class m200507_061808_etp extends Migration
             'id'            => $this->bigPrimaryKey(),
 
             'number'        => $this->integer()->notNull(),
-            'organizer_id'  => $this->bigInteger(),
+            'organizer_id'  => $this->bigInteger()->notNull(),
 
             'created_at'    => $this->integer()->notNull(),
             'updated_at'    => $this->integer()->notNull(),
         ]);
 
-        $this->addForeignKey('fk-lot-torg', self::TABLE, 'torg_id', '{{%torg}}', 'id', 'restrict', 'restrict');
+        $this->addForeignKey('fk-etp-organization', self::TABLE, 'organizer_id', '{{%organization}}', 'id', 'restrict', 'restrict');
 
-		$this->addCommentOnColumn(self::TABLE, 'torg_id', 'Торг, к которому принадлежит лот');
-		$this->addCommentOnColumn(self::TABLE, 'title', 'Заголовок лота, как правило это первые слова Описания');
-		$this->addCommentOnColumn(self::TABLE, 'description', 'Описание');
-		$this->addCommentOnColumn(self::TABLE, 'start_price', 'Начальная цена');
-		$this->addCommentOnColumn(self::TABLE, 'step', 'Шаг уменьшения цены');
-		$this->addCommentOnColumn(self::TABLE, 'step_measure', 'Мера шага - сумма, %');
-		$this->addCommentOnColumn(self::TABLE, 'deposite', 'Размер задатка за лот');
-		$this->addCommentOnColumn(self::TABLE, 'deposite_measure', 'Мера задатка - сумма, %');
-		$this->addCommentOnColumn(self::TABLE, 'status', 'Статус');
-		$this->addCommentOnColumn(self::TABLE, 'reason', 'Причина статуса');
+		$this->addCommentOnColumn(self::TABLE, 'number', 'Номер торговой площадки');
+		$this->addCommentOnColumn(self::TABLE, 'organizer_id', 'Компания торговой площадки');
+
     }
 
     /**
@@ -43,7 +36,7 @@ class m200507_061808_etp extends Migration
      */
     public function safeDown()
     {
-        $this->dropForeignKey('fk-lot-torg', self::TABLE);
+        $this->dropForeignKey('fk-etp-organization', self::TABLE);
         $this->dropTable(self::TABLE);
     }
 }
