910c7d4186ef9e486efb369d9019bbf36c5ab4a0
root <sergmoro1@ya.ru>
2020-05-09 23:52:32 +0300

    Tring to optimize Torg filling.

--- console/migrations/fill/m200507_121529_torg_fill.php
+++ console/migrations/fill/m200507_121529_torg_fill.php
@@ -15,7 +15,7 @@ class m200507_121529_torg_fill extends Migration
     use Keeper;
     
     const TABLE  = '{{%torg}}';
-    const OFFSET = 1000;
+    const POOLE = 100;
     
     private static $offer_convertor = [
         "Конкурс" => 4,
@@ -32,7 +32,9 @@ class m200507_121529_torg_fill extends Migration
         $select = $db->createCommand(
             'SELECT id FROM "eiLot".torgs ORDER BY "torgs".id'
         );
-        $rows = $select->queryAll();
+        $ids = $select->queryAll();
+        
+        $poole = [];
         
         $torgs = [];
         $torgs_debtor = [];
@@ -40,85 +42,84 @@ class m200507_121529_torg_fill extends Migration
         $torgs_drawish = [];
    
         // добавление информации о торгах
-        for($i = 1; $i <= count($rows); $i++) {
-
-            if (($b = floor($i / self::OFFSET)) && (($b * self::OFFSET) == $i)) {
-
-                $this->batchInsert(self::TABLE, ['id', 'property', 'description', 'started_at', 'end_at', 'completed_at', 'published_at', 'offer', 'created_at', 'updated_at'], $torgs);
-                $this->batchInsert('{{%torg_debtor}}', ['torg_id', 'etp_id', 'bankrupt_id', 'manager_id', 'case_id'], $torgs_debtor);
-                $this->batchInsert('{{%torg_pledge}}', ['torg_id', 'owner_id', 'user_id'], $torgs_pledge);
-                $this->batchInsert('{{%torg_drawish}}', ['torg_id', 'manager_id'], $torgs_drawish);
-
-                $torgs = [];
-                $torgs_debtor = [];
-                $torgs_pledge = [];
-                $torgs_drawish = [];
+        foreach($ids as $id)
+        {
+            
+            if (count($poole) < self::POOLE) {
+                $poole[] = $id['id'];
+                continue;
             }
 
-            $torg_id = $rows[$i - 1]['id'];
-
-            $select = $db->createCommand(
-                'SELECT * FROM "eiLot".torgs WHERE id = ' . $torg_id
+            $q = $db->createCommand(
+                'SELECT * FROM "eiLot".torgs WHERE id IN (' . implode(',', $poole) . ')'
             );
-            $row = $select->queryAll();
-            $row = $row[0];
 
-            $created_at = strtotime($row['createdAt']);
-            $updated_at = strtotime($row['updatedAt']);
-            $property   = $row['typeId'];
-            
-            $obj = json_decode($row['info']);
-            
-            // Torg
-            $t = [
-                'id'           => $torg_id,
-                'property'     => $property,
-                'description'  => $row['description'],
-                'started_at'   => ($row['startDate'] ? strtotime($row['startDate']) : null),
-                'end_at'       => ($row['endDate'] ? strtotime($row['endDate']) : null),
-                'completed_at' => ($row['completeDate'] ? strtotime($row['completeDate']) : null),
-                'published_at' => ($row['publishedDate'] ? strtotime($row['publishedDate']) : null),
-                'offer'        => self::$offer_convertor[$row['tradeType']],
+            $rows = $q->queryAll();
+            $poole = [];
 
-                'created_at'   => $created_at,
-                'updated_at'   => $updated_at,
-            ];
-            $torg = new Torg($t);
-            
-            if ($this->validateAndKeep($torg, $torgs, $t)) {
-                if ($property == Torg::PROPERTY_BANKRUPT) {
-                    $td = [
-                        'torg_id'     => $torg_id,
-                        'etp_id'      => $row['etpId'],
-                        'bankrupt_id' => $row['bankruptId'],
-                        'manager_id'  => $row['publisherId'],
-                        'case_id'     => $row['caseId'],
-                    ];
-                    $torg_debtor = new TorgDebtor($td);
-                    $this->validateAndKeep($torg_debtor, $torgs_debtor, $td);
-                } elseif ($property == Torg::PROPERTY_ZALOG) {
-                    $tp = [
-                        'torg_id'  => $torg_id,
-                        'user_id'  => $row['publisherId'],
-                        'owner_id' => $row['bankruptId'],
-                    ];
-                    $torg_pledge = new TorgPledge($tp);
-                    $this->validateAndKeep($torg_pledge, $torgs_pledge, $tp);
-                } else {
-                    $td = [
-                        'torg_id'    => $torg_id,
-                        'manager_id' => $row['publisherId'],
-                    ];
-                    $torg_drawish = new TorgDrawish($td);
-                    $this->validateAndKeep($torg_drawish, $torgs_drowish, $td);
+            foreach($rows as $row)
+            {
+                $torg_id = $row['id'];
+
+                $created_at = strtotime($row['createdAt']);
+                $updated_at = strtotime($row['updatedAt']);
+                $property   = $row['typeId'];
+                
+                // Torg
+                $t = [
+                    'id'           => $torg_id,
+                    'property'     => $property,
+                    'description'  => $row['description'],
+                    'started_at'   => ($row['startDate'] ? strtotime($row['startDate']) : null),
+                    'end_at'       => ($row['endDate'] ? strtotime($row['endDate']) : null),
+                    'completed_at' => ($row['completeDate'] ? strtotime($row['completeDate']) : null),
+                    'published_at' => ($row['publishedDate'] ? strtotime($row['publishedDate']) : null),
+                    'offer'        => self::$offer_convertor[$row['tradeType']],
+
+                    'created_at'   => $created_at,
+                    'updated_at'   => $updated_at,
+                ];
+                $torg = new Torg($t);
+                
+                if ($this->validateAndKeep($torg, $torgs, $t)) {
+                    if ($property == Torg::PROPERTY_BANKRUPT) {
+                        $td = [
+                            'torg_id'     => $torg_id,
+                            'etp_id'      => $row['etpId'],
+                            'bankrupt_id' => $row['bankruptId'],
+                            'manager_id'  => $row['publisherId'],
+                            'case_id'     => $row['caseId'],
+                        ];
+                        $torg_debtor = new TorgDebtor($td);
+                        $this->validateAndKeep($torg_debtor, $torgs_debtor, $td);
+                    } elseif ($property == Torg::PROPERTY_ZALOG) {
+                        $tp = [
+                            'torg_id'  => $torg_id,
+                            'user_id'  => $row['publisherId'],
+                            'owner_id' => $row['bankruptId'],
+                        ];
+                        $torg_pledge = new TorgPledge($tp);
+                        $this->validateAndKeep($torg_pledge, $torgs_pledge, $tp);
+                    } else {
+                        $td = [
+                            'torg_id'    => $torg_id,
+                            'manager_id' => $row['publisherId'],
+                        ];
+                        $torg_drawish = new TorgDrawish($td);
+                        $this->validateAndKeep($torg_drawish, $torgs_drowish, $td);
+                    }
                 }
             }
-        }
-        if (count($torgs) > 0) {
+
             $this->batchInsert(self::TABLE, ['id', 'property', 'description', 'started_at', 'end_at', 'completed_at', 'published_at', 'offer', 'created_at', 'updated_at'], $torgs);
             $this->batchInsert('{{%torg_debtor}}', ['torg_id', 'etp_id', 'bankrupt_id', 'manager_id', 'case_id'], $torgs_debtor);
             $this->batchInsert('{{%torg_pledge}}', ['torg_id', 'owner_id', 'user_id'], $torgs_pledge);
             $this->batchInsert('{{%torg_drawish}}', ['torg_id', 'manager_id'], $torgs_drawish);
+
+            $torgs = [];
+            $torgs_debtor = [];
+            $torgs_pledge = [];
+            $torgs_drawish = [];
         }
     }
 
