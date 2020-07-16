ec14ec37788d3d366e5536a2f4040e364d51ecd6
prodvair <far.almazi@gmail.com>
2020-07-16 19:23:19 +0300

    fixed arrest bankrupt search

--- backend/models/ImportFIleForm.php
+++ backend/models/ImportFIleForm.php
@@ -36,103 +36,103 @@ class ImportFIleForm extends Model
         $loadCount = 0;
 
         while(!empty($sheetData[$baseRow]['B']) || $sheetData[$baseRow]['B'] != null){
+            $whereCad   = ['or'];
+            $whereF     = ['and'];
 
-        // $rowsData = explode("\r\n", str_replace('"', "'", $this->fileImport));
-        // foreach ($rowsData as $id => $rowData) {
-        //     $colsData[$id] = explode("\t", $rowData);
-        // }
-        // $searchData = [];
-        // $i = 0;
+            if ($cad = $this->getCadastre((string)$sheetData[$baseRow]['D'])) {
+                $orWhereCad = ['or'];
 
-        // foreach ($colsData as $row => $value) {
-            // if ($row != 0 && !empty($value[3]) && !empty($sheetData[$baseRow]['C']) && !empty($value[1])) {
-                $whereCad   = ['or'];
-                $whereF     = ['and'];
-
-                if ($cad = $this->getCadastre((string)$sheetData[$baseRow]['D'])) {
-                    $whereCad[] = ['like', 'lotPropName', $cad];
-                    $whereF[] = ['like', 'lotPropName', $cad];
-                }
-                if ($vin = $this->getVin((string)$sheetData[$baseRow]['D'])) {
-                    $whereCad[] = ['like', 'lotPropName', $vin];
-                    $whereF[] = ['like', 'lotPropName', $vin];
-                }
-
-                $whereFirst = $whereSecond = $whereCad;
-
-                // ----------------------------
-                $whereS     = ['or'];
-                $whereS[]   = ['like', 'lotPropName', (string)$sheetData[$baseRow]['B']];
-                
-                $orWhere = ['or'];
-                if (strpos(mb_strtolower((string)$sheetData[$baseRow]['C'],'UTF-8'), 'ооо') !== false || strpos(mb_strtolower((string)$sheetData[$baseRow]['C'],'UTF-8'), 'оао') !== false) {
-                    $orWhere[] = ['like', 'lotPropName', (string)$sheetData[$baseRow]['C']];
+                if (count($cad) > 1) {
+                    foreach ($cad as $value) {
+                        $orWhereCad[] = ['like', 'lotPropName', $value];
+                    }
+                    $whereF[] = $whereSecond = $orWhereCad;
                 } else {
-                    $fio = str_replace(['ИП', 'ип'], '', (string)$sheetData[$baseRow]['C']);
-                    $names = explode(' ',  ltrim($fio));
-                    $orWhere[] = ['like', 'lotPropName', $names[0]];
-                    $orWhere[] = ['like', 'lotPropName', $names[0].' '.mb_substr($names[1],0,1,'UTF-8').'.'.mb_substr($names[2],0,1,'UTF-8').'.'];
-                    $orWhere[] = ['like', 'lotPropName', $names[0].' '.mb_substr($names[1],0,1,'UTF-8').'. '.mb_substr($names[2],0,1,'UTF-8').'.'];
-                    $orWhere[] = ['like', 'lotPropName', $names[0].' '.mb_substr($names[1],0,1,'UTF-8').' '.mb_substr($names[2],0,1,'UTF-8')];
-                    $orWhere[] = ['like', 'lotPropName', $names[0].' '.mb_substr($names[1],0,1,'UTF-8').mb_substr($names[2],0,1,'UTF-8')];
+                    $whereF[] = $whereSecond = ['like', 'lotPropName', $cad[0]];
                 }
-                $whereF[] = $whereS[] = $orWhere;
-                // ----------------------------
+            }
+            if ($vin = $this->getVin((string)$sheetData[$baseRow]['D'])) {
+                $whereVin = ['like', 'lotPropName', $vin];
+                $whereF[] = $whereSecond = $whereVin;
+            }
 
-                $whereSecond[]  = $whereS;
-                $whereFirst[]   = $whereF;
+            // ----------------------------
+            $whereS     = ['or'];
+            $whereS[]   = ['like', 'lotPropName', (string)$sheetData[$baseRow]['B']];
+            
+            $orWhere = ['or'];
+            if (strpos(mb_strtolower((string)$sheetData[$baseRow]['C'],'UTF-8'), 'ооо') !== false || strpos(mb_strtolower((string)$sheetData[$baseRow]['C'],'UTF-8'), 'оао') !== false) {
+                $orWhere[] = ['like', 'lotPropName', (string)$sheetData[$baseRow]['C']];
+            } else {
+                $fio = str_replace(['ИП ', 'ип ','ИП', 'ип'], ['', '', '', ''], (string)$sheetData[$baseRow]['C']);
+                $names = explode(' ',  ltrim($fio));
+                $orWhere[] = ['like', 'lotPropName', $names[0]];
 
-                $lotsFirst = LotsArrest::find()->joinWith('torgs')->where($whereFirst)->orderBy('trgPublished ASC')->limit(5)->all();
-                $lotsSecond = LotsArrest::find()->joinWith('torgs')->where($whereSecond)->orderBy('trgPublished ASC')->limit(30)->all();
+                $orWhere[] = ['like', 'lotPropName', $names[0].' '.mb_substr($names[1],0,1,'UTF-8').'.'.mb_substr($names[2],0,1,'UTF-8').'.'];
+                $orWhere[] = ['like', 'lotPropName', $names[0].' '.mb_substr($names[1],0,1,'UTF-8').'. '.mb_substr($names[2],0,1,'UTF-8').'.'];
+                $orWhere[] = ['like', 'lotPropName', $names[0].' '.mb_substr($names[1],0,1,'UTF-8').' '.mb_substr($names[2],0,1,'UTF-8')];
+                $orWhere[] = ['like', 'lotPropName', $names[0].' '.mb_substr($names[1],0,1,'UTF-8').mb_substr($names[2],0,1,'UTF-8')];
                 
-                foreach ($lotsFirst as $key => $lot) {
-                    $result['first'][] = [
-                        'inn'   => (string)$sheetData[$baseRow]['B'],
-                        'name'  => (string)$sheetData[$baseRow]['C'],
-                        'id'            => $lot->lotId,
-                        'title'         => $lot->lotPropName,
-                        'torg'          => 'http://torgi.gov.ru',
-                        'repeat'        => (($key != 0)? 'Да' : 'Нет' ),
-                        'publication'   => $lot->torgs->trgPublished,
-                        'auction'       => $lot->torgs->trgBidAuctionDate,
-                        'form'          => $lot->torgs->trgBidFormName,
-                        'winner'        => $lot->lotWinnerName,
-                        'price'         => $lot->lotStartPrice,
-                        'url'           => $lot->torgs->trgNotificationUrl,
-                    ];
-                }
-                foreach ($lotsSecond as $key => $lot) {
-                    $result['second'][] = [
-                        'inn'   => (string)$sheetData[$baseRow]['B'],
-                        'name'  => (string)$sheetData[$baseRow]['C'],
-                        'id'            => $lot->lotId,
-                        'title'         => $lot->lotPropName,
-                        'torg'          => 'http://torgi.gov.ru',
-                        'repeat'        => (($key != 0)? 'Да' : 'Нет' ),
-                        'publication'   => $lot->torgs->trgPublished,
-                        'auction'       => $lot->torgs->trgBidAuctionDate,
-                        'form'          => $lot->torgs->trgBidFormName,
-                        'winner'        => $lot->lotWinnerName,
-                        'price'         => $lot->lotStartPrice,
-                        'url'           => $lot->torgs->trgNotificationUrl,
-                    ];
-                }
-                
-            // }
+                $orWhere[] = ['like', 'lotPropName', mb_substr($names[1],0,1,'UTF-8').'.'.mb_substr($names[2],0,1,'UTF-8').'. '.$names[0]];
+                $orWhere[] = ['like', 'lotPropName', mb_substr($names[1],0,1,'UTF-8').'. '.mb_substr($names[2],0,1,'UTF-8').'. '.$names[0]];
+                $orWhere[] = ['like', 'lotPropName', mb_substr($names[1],0,1,'UTF-8').' '.mb_substr($names[2],0,1,'UTF-8').' '.$names[0]];
+                $orWhere[] = ['like', 'lotPropName', mb_substr($names[1],0,1,'UTF-8').mb_substr($names[2],0,1,'UTF-8').' '.$names[0]];
+            }
+            $whereF[] = $whereS[] = $orWhere;
+            // ----------------------------
+
+            $whereSecond[]  = $whereS;
+            $whereFirst   = $whereF;
+
+            $lotsFirst = LotsArrest::find()->joinWith('torgs')->where($whereFirst)->orderBy('trgPublished ASC')->limit(30)->all();
+            $lotsSecond = LotsArrest::find()->joinWith('torgs')->where($whereSecond)->orderBy('trgPublished ASC')->limit(30)->all();
+            
+            foreach ($lotsFirst as $key => $lot) {
+                $result['first'][] = [
+                    'inn'   => (string)$sheetData[$baseRow]['B'],
+                    'name'  => (string)$sheetData[$baseRow]['C'],
+                    'id'            => $lot->lotId,
+                    'title'         => $lot->lotPropName,
+                    'torg'          => 'http://torgi.gov.ru',
+                    'repeat'        => (($key != 0)? 'Да' : 'Нет' ),
+                    'publication'   => $lot->torgs->trgPublished,
+                    'auction'       => $lot->torgs->trgBidAuctionDate,
+                    'form'          => $lot->torgs->trgBidFormName,
+                    'winner'        => $lot->lotWinnerName,
+                    'price'         => $lot->lotStartPrice,
+                    'url'           => $lot->torgs->trgNotificationUrl,
+                ];
+            }
+            foreach ($lotsSecond as $key => $lot) {
+                $result['second'][] = [
+                    'inn'   => (string)$sheetData[$baseRow]['B'],
+                    'name'  => (string)$sheetData[$baseRow]['C'],
+                    'id'            => $lot->lotId,
+                    'title'         => $lot->lotPropName,
+                    'torg'          => 'http://torgi.gov.ru',
+                    'repeat'        => (($key != 0)? 'Да' : 'Нет' ),
+                    'publication'   => $lot->torgs->trgPublished,
+                    'auction'       => $lot->torgs->trgBidAuctionDate,
+                    'form'          => $lot->torgs->trgBidFormName,
+                    'winner'        => $lot->lotWinnerName,
+                    'price'         => $lot->lotStartPrice,
+                    'url'           => $lot->torgs->trgNotificationUrl,
+                ];
+            }
+            
             $baseRow++;
         }
         return $result;
     }
     public function getCadastre($text) 
     {
-        $kadastr_check = preg_match("/[0-9]{2}:[0-9]{2}:[0-9]{6,7}:[0-9]{1,35}/", $text, $kadastr);
+        $kadastr_check = preg_match_all("/[0-9]{2}:[0-9]{2}:[0-9]{6,7}:[0-9]{1,35}/", $text, $kadastr);
         return ($kadastr_check)? $kadastr[0] : false;
     }
     public function getVin($text) 
     {
-        $vin_text = str_replace(['WIN', 'VIN'], ['', ''],$text);
-        $vin_check = preg_match("/[ABCDEFGHJKLMNPRSTUVWXYZ,0-9]{17}/", $vin_text, $vin_t);
-        $vin_c = preg_match("/[\w\s\d]+/u", $vin_t[0], $vin);
+        $vin_text = str_replace(['WIN', 'VIN', 'ВИН', 'win', 'vin', 'вин'], ['', '', '', '', '', ''],$text);
+        $vin_check = preg_match("/[A-HJ-NPR-Z0-9]{17}/", $vin_text, $vin);
         return ($vin_check)? $vin[0] : false;
     }
 }
--- console/controllers/TestController.php
+++ console/controllers/TestController.php
@@ -31,9 +31,13 @@ class TestController extends Controller
         return $lots;
     }
 
-    public function actionAddress()
+    public function actionCad()
     {
-        # code...
+        $text = 'Полуприцеп Renders RSCC, VINYA5B3Z1N639B94500, 1996г.в., НПЦ 238 000р. Грузовой седельный тягач International 92001, 2002 г.в., VIN 3HSCEAMR93N054641, НПЦ 770 000р. Грузовой тягач седельный MAN TGA 19.390 4X2 BLS-WW, 2008г.в., VIN WMAHW7ZZ19P011656, НПЦ 1 760 000р.';
+        $vin_text = str_replace(['WIN', 'VIN', 'ВИН', 'win', 'vin', 'вин'], '',$text);
+        $vin_check = preg_match("/[A-HJ-NPR-Z0-9]{17}/", $vin_text, $vin);
+        // return ($kadastr_check)? $kadastr[0] : false;
+        var_dump($vin_text,$vin_check, $vin);
     }
     // php yii test/arrest
 }
