ca0a0a1a6700b078a8b0b6eb416a3aee836347c5
prodvair <far.almazi@gmail.com>
2020-07-31 10:24:05 +0300

    fixed report

--- frontend/modules/components/views/reportBlock.php
+++ frontend/modules/components/views/reportBlock.php
@@ -8,86 +8,146 @@ use yii\helpers\Html;
 /* @var $lot Lot */
 
 ?>
-<style>
-    .report-block-wrapper {
-        background-color: #e4f3f8;
-    }
-
-    .ei-green {
-        color: #077751;
-    }
-
-    .ei-report-int {
-        font-size: 3.2em;
-    }
-
-</style>
-<div class="col-md-12 mb-30">
-    <?php foreach ($reports as $report) : ?>
-        <div class="col-md-12 report-block-wrapper mb-20">
-            <div class="row">
-                <div class="col-md-6"><p class="ei-green"><b>Отчет эксперта</b></p></div>
-                <div class="col-md-6 text-right"><p class="ei-green">Лот № <?= $lot->id ?></p></div>
-            </div>
-            <div class="col-md-12 mt-40">
-                <div class="row">
-                    <div class="col-md-8">
-                        <div class="col-md-12">
-                            <div class="row mb-5">
-                                <div class="col-md-5 bg-white mr-5">
-                                    <?= $report->user->getFullName() ?>
-                                </div>
-                                <div class="col-md-3 bg-white mr-5">
-                                    <span>Риски</span>
-                                    <span class="ei-green ei-report-int"><?= $report->risk ?></span>&nbsp;/10
-                                </div>
-                                <div class="col-md-3 bg-white mr-5">
-                                    <span>Интерес</span>
-                                    <p><span class="ei-green ei-report-int"><?= $report->attraction ?></span>&nbsp;/10
-                                    </p>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-
-                <div class="col-md-12 mt-30">
-                    <div class="row">
-                        <div class="col-md-8 bg-white">
-                            <?php
-                            $image = $report->getImage('original');
-                            if ($image) : ?>
-                                <div class="fotorama mt-20 mb-40" data-allowfullscreen="true" data-nav="thumbs"
-                                     data-arrows="always" data-click="true">
-                                    <?php
-                                    while ($image) {
-                                        echo Html::img($image, ['alt' => 'Images']);
-                                        $image = $report->getNextImage('original');
-                                    }
-                                    ?>
-                                </div>
-                            <?php endif; ?>
-                        </div>
-                        <div class="col-md-4">
-                            <div class="col-md-12">
-                                <b><?= $report->title ?></b>
-                            </div>
-                            <div class="col-md-12">
-                                <?= $report->content ?>
-                            </div>
-                            <div class="col-md-12 mt-20">
-                                <p class="ei-green"><b>Цена: <?= $report->cost ?> руб.</b></p>
-                            </div>
-                            <div class="col-md-12 mt-20 mb-20">
-                                <a class="btn btn-primary btn-block text-white">Купить отчет</a>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-
-            </div>
-            <div class="col-md-12">&nbsp;</div>
+<?php foreach ($reports as $report) : ?>
+    <div class="report mb-40">
+        <div class="report__head">
+            <div class="report__head__name">Отчет эксперта</div>
+            <div class="report__head__number">Лот № <?= $lot->id ?></div>
         </div>
+        <div class="report__body row">
+            <div class="col-md-6 report__body__expert">
+                <?=$report->user->getAvatar(['class'=> "report__body__expert-img"])?>
+                <span class="report__body__expert__name">
+                    <?=$report->user->profile->first_name?>
+                    <br>
+                    <?=$report->user->profile->last_name?>
+                </span>
+            </div>
+            <div class="col-md-6 report__body__diogramm">
+                <div class="report__body__diogramm__green">
+                    <svg class="report__body__diogramm__green-svg" width="70" height="70">
+                        <circle 
+                            class="report__body__diogramm__green-circle--back" 
+                            stroke="#BDBDBD" 
+                            stroke-width="5" 
+                            cx="35" 
+                            cy="35" 
+                            r="30" 
+                            fill="transparent"
+                        />
+                        <circle 
+                            class="report__body__diogramm__green-circle" 
+                            id="green-circle-<?=$report->id?>"
+                            stroke="#077751" 
+                            stroke-width="5" 
+                            cx="35" 
+                            cy="35" 
+                            r="30" 
+                            fill="transparent"
+                        />
+                    </svg>
 
-    <?php endforeach; ?>
-</div>
+                    <div class="report__body__diogramm__green__info">
+                        <div class="report__body__diogramm__green__info__number">
+                            <span><?= $report->attraction ?></span>/10
+                        </div>
+                        <div class="report__body__diogramm__green__info__name">
+                            Интерес
+                        </div>
+                    </div>
+
+                    <script>
+                        // не забывать подтсавлять номер для id
+                        var circle = document.querySelector('#green-circle-<?=$report->id?>');
+                        var r = circle.r.baseVal.value;
+                        var circumference = 2 * Math.PI * r;
+
+                        circle.style.strokeDasharray = `${circumference} ${circumference}`;
+                        circle.style.strokeDashoffset = circumference;
+
+                        function setProgress(percent) {
+                            const offset = circumference - percent / 100 * circumference;
+                            circle.style.strokeDashoffset = offset;
+                        }
+
+                        setProgress(<?= $report->attraction ?>0);
+                    </script>
+                </div>
+
+                <div class="report__body__diogramm__red">
+                    <svg class="report__body__diogramm__red-svg" width="70" height="70">
+                        <circle 
+                            class="report__body__diogramm__red-circle--back" 
+                            stroke="#BDBDBD" 
+                            stroke-width="5" 
+                            cx="35" 
+                            cy="35" 
+                            r="30" 
+                            fill="transparent"
+                        />
+                        <circle 
+                            class="report__body__diogramm__red-circle" 
+                            id="red-circle-<?=$report->id?>"
+                            stroke="#EB5757" 
+                            stroke-width="5" 
+                            cx="35" 
+                            cy="35" 
+                            r="30" 
+                            fill="transparent"
+                        />
+                    </svg>
+
+                    <div class="report__body__diogramm__red__info">
+                        <div class="report__body__diogramm__red__info__number">
+                            <span><?= $report->risk ?></span>/10
+                        </div>
+                        <div class="report__body__diogramm__red__info__name">
+                            риск
+                        </div>
+                    </div>
+
+                    <script>
+                        // не забывать подтсавлять номер для id
+                        var circle = document.querySelector('#red-circle-<?=$report->id?>');
+                        var r = circle.r.baseVal.value;
+                        var circumference = 2 * Math.PI * r;
+
+                        circle.style.strokeDasharray = `${circumference} ${circumference}`;
+                        circle.style.strokeDashoffset = circumference;
+
+                        function setProgress(percent) {
+                            const offset = circumference - percent / 100 * circumference;
+                            circle.style.strokeDashoffset = offset;
+                        }
+
+                        setProgress(<?= $report->risk ?>0);
+                    </script>
+                </div>
+                
+            </div>
+            <div class="col-md-6 report__body__images">
+                <div class="report__body__images__slider">
+                    <?php
+                    $image = $report->getImage('original');
+                    if ($image) : 
+                        while ($image) { ?>
+                        <div class="report__body__images__slider__item">
+                            <img src="$image" alt="Images">
+                        </div>
+                    <?php 
+                            $image = $report->getNextImage('original');
+                        } 
+                    endif; 
+                    ?>
+                </div>
+            </div>
+            <div class="col-md-6 report__body__info">
+                <div class="report__body__info__title mb-20"><?= $report->title ?></div>
+                <p class="report__body__info__text mb-30"><?= $report->content ?></p>
+                <div class="report__body__info__price mb-20">Цена: <?= $report->cost ?> руб.</div>
+                <a class="btn btn-primary btn-block text-white">Купить отчет</a>
+            </div>
+        </div>
+    </div>
+<?php endforeach; ?>
+
